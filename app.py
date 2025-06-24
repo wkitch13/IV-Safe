7096254469a1b67752055848ee65b819480c210b
Wren Kitchings <WrenKitchings@Wrens-MacBook-Air.local>
2025-06-24 11:57:47 -0700

    creating different pages and adding favicon

---
+++ app.py
@@ -0,0 +1,27 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__) # for hosting the website
+
+@app.route('/') # specifies to run this method when the URL contains whatever is in the '',
+# in this case it is the homepage, because it runs whenever there is just a / after the URL
+def homepage(): # homepage method
+    return render_template("homepage.html")
+
+@app.route('/swelling')
+def swelling():
+    return render_template("swelling.html")
+
+@app.route('/leakage')
+def leakage():
+    return render_template("leakage.html")
+
+@app.route('/team') # meet the team
+def team():
+    return render_template("team.html")
+
+@app.route('/about') # contact, support us, publications, etc.
+def about():
+    return render_template("about.html")
+
+if __name__ == '__main__':
+    app.run(debug=True)
