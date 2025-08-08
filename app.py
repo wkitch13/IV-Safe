1af9c44216e04f13e889daa1ef77aaab55878591
Wren Kitchings <wkitch@uw.edu>
2025-08-08 10:10:15 -0700

    changed the file structure to support local development.

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__) # for hosting the website
 @app.route('/') # specifies to run this method when the URL contains whatever is in the '',
 # in this case it is the homepage, because it runs whenever there is just a / after the URL
 def homepage(): # homepage method
-    return render_template("homepage.html")
+    return render_template("index.html")
 
 @app.route('/swelling')
 def swelling():
